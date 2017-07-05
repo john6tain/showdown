d7326bdce982ae2539f3402a260e3ad66c8361a1
Alex <alexchobanov@gmail.com>
2017-07-05 17:50:22 +0300

    Added Alt to dependencies. Created alt.js, config file.

--- src/components/app/NavBar.js
+++ src/components/app/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Item = (props) => (
     <LinkContainer to={ props.url }>
@@ -14,14 +15,10 @@ export default class NavBar extends React.Component {
             <Navbar>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        {/*<a href="#">React-Bootstrap</a>*/}
+                        <Link to="/">Showdown</Link>
                     </Navbar.Brand>
                 </Navbar.Header>
                 <Nav>
-                    <Item url="/user" eventKey={ 1 }>User</Item>
-                    <LinkContainer to="/penka/2">
-                        <NavItem eventKey={2} href="/user/2">Link</NavItem>
-                    </LinkContainer>
                     <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
                         <MenuItem eventKey={3.1}>Action</MenuItem>
                         <MenuItem eventKey={3.2}>Another action</MenuItem>
