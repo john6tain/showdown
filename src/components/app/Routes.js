3acd5db16da2d49f0bc28f526c1f6d9c4315fb96
Alex <alexchobanov@gmail.com>
2017-07-07 11:14:52 +0300

    Rolling back TopTen from master. Put in under TopTen branch`

--- src/components/app/Routes.js
+++ src/components/app/Routes.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import TopTenContainer from'../containers/TopTenContainer'
 import ShowdownContainer from '../containers/ShowdownContainer';
 
 const Routes = () => (
     <Switch>
         <Route exact path='/' component={ ShowdownContainer } />
-        <Route path='/topten' component={ TopTenContainer } />
     </Switch>
 );
 
