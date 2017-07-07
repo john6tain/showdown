75d7c0e09ae758129f93bc404262d0123defd565
Alex <alexchobanov@gmail.com>
2017-07-08 01:11:36 +0300

    Added end-points to get showdown, add combatant and vote on showdown. Add works, but getting showdown is problematic due to the necessety of being random.

--- server/controllers/combatant.js
+++ server/controllers/combatant.js
@@ -8,7 +8,7 @@ module.exports = {
         let combatantData = {
             name: req.body.name,
             description: req.body.description,
-            imageUrl: req.body.imageUrl
+            imageUrl: req.body.image
         };
         Combatant.create(combatantData).then(combatant => {
             if (!combatant) {
@@ -18,19 +18,36 @@ module.exports = {
             res.status(200).end();
         });
     },
-    showdown:  (req, res) => {
-        User.findById(TMP_USER_ID).then(user => {
-            Combatant
-                .find({ random: { $near: [Math.random(), 0] } })
-                .where('upVotes').nin(user.upVotedCombatants)
-                .limit(2)
-                .then(combatants => {
-                    if (combatants.length !== 2 ) {
-                        return res.status(200).send({ combatants: [], message: 'Showtime over. No more combatants.' });
-                    }
+    showdown: {
+        get: (req, res) => {
+            User.findById(TMP_USER_ID).then(user => {
+                Combatant
+                    .find({ random: { $near: [Math.random(), 0] } })
+                    .where('_id').nin([ user._id ])
+                    .limit(2)
+                    .then(combatants => {
+                        if (combatants.length !== 2) {
+                            return res.status(200).send({
+                                combatants: [],
+                                message: 'Showtime over. No more combatants.'
+                            });
+                        }
 
-                    res.status(200).send({ combatants });
-                });
-        });
+                        res.status(200).send({combatants});
+                    });
+            });
+        },
+        upVote: (req, res) => {
+            let combatantId = req.body.combatantId;
+            Combatant.findById(combatantId).then(combatant => {
+                if (!combatant) {
+                    return res.status(500).send({ message: 'Internal server error. No db connection or db corruption' });
+                }
+
+                combatant.upVotes += 1;
+                combatant.save();
+                res.status(200).end();
+            })
+        }
     },
 };
--- server/models/Combatant.js
+++ server/models/Combatant.js
@@ -4,7 +4,7 @@ let combatantSchema = mongoose.Schema({
     imageUrl: { type: mongoose.Schema.Types.String, required: true },
     name: { type: mongoose.Schema.Types.String, required: true },
     description: { type: mongoose.Schema.Types.String },
-    upVotes: { type: mongoose.Schema.Types.ObjectId },
+    upVotes: { type: mongoose.Schema.Types.Number, default: 0 },
     dateCreated: { type: mongoose.Schema.Types.Date, default: Date.now },
     ownerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
