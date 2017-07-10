5ba70cc80d4242857d2ccf1fbc793997d5223c2f
unknown <john6tain.tube@gmail.com>
2017-07-10 19:18:22 +0300

    Server only fix

--- app/index.js
+++ app/index.js
@@ -4,11 +4,11 @@ const express = require('express');
 const port =  process.env.PORT || 3001;
 
 const config = require('./config/config');
-const database = require('./config/database.js');
+const database = require('./config/database');
 
 let app = express();
 
-let environment = process.env.NODE_ENV || 'production';
+let environment = 'production';
 database(config[environment]);
 require('./config/express')(app, config[environment]);
 require('./config/routes')(app);
